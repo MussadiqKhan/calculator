da96ee5f4ec63c34328b4ba97e5b612c49203409
MussadiqKhan <34172073+MussadiqKhan@users.noreply.github.com>
2019-08-07 23:17:17 +0500

    Updates

---
+++ precache-manifest.1b4d3ce529482e311fd309c2243b46bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "176a27de3f136c224ad85287fe3d0a55",
+    "url": "/calculator/index.html"
+  },
+  {
+    "revision": "d8664db43bb99892e0bc",
+    "url": "/calculator/static/css/main.200f9703.chunk.css"
+  },
+  {
+    "revision": "39efdaddde20d46dac56",
+    "url": "/calculator/static/js/2.1b5bff57.chunk.js"
+  },
+  {
+    "revision": "d8664db43bb99892e0bc",
+    "url": "/calculator/static/js/main.606f8092.chunk.js"
+  },
+  {
+    "revision": "2fe8cbdbbea1fbf116e6",
+    "url": "/calculator/static/js/runtime~main.2539f434.js"
+  }
+]);
